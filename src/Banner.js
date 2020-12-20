90dc4bd92f1d4b0c488d55442f5e4d60d790d17f
michael dilulo <snoleopard01001101@gmail.com>
2020-12-20 10:18:44 -0800

    H1 Text shows when button is clicked

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Banner.css"
-import {Button} from "@material-ui/core"
+import { Button } from "@material-ui/core"
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false)
+
+
     return (
         <div className="banner">
             <div className="banner__search">
-            <Button variant="outlined" className="banner__searchButton">Search Dates</Button>
+                {showSearch && <h1>SHOW DATE PICKER</h1>}
+            <Button variant="outlined" className="banner__searchButton" onClick={() => setShowSearch(!showSearch)}>Search Dates</Button>
             </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
