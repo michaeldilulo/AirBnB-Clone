23494f6bbf0aba7bfc2402c472d2d0e61712cc05
michael dilulo <snoleopard01001101@gmail.com>
2020-12-20 10:34:11 -0800

    Date Range Picker and handleSelect

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import "./Search.css"
+import { DateRangePicker } from "react-date-range"
+// main style file
+import "react-date-range/dist/styles.css";
+// these css file
+import "react-date-range/dist/theme/default.css"
+
 
 function Search() {
+    const [startDate, setStartDate] = useState(newDate());
+    const [endDate, setEndDate] = useState(newDate())
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        // recommended to use for date picker 
+        key: "selection"
+    };
+
+    function handleSelect(ranges) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate)
+    }
+
+
     return (
         <div className="search">
-            
-        </div>
+            <DateRangePicker ranges={
+                [selectionRange]
+            } onChange={handleSelect} />
+         </div>
     )
 }
 
