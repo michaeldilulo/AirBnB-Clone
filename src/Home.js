e04128a28de27e5b8552ce5196284c31dbb6f4a8
michael dilulo <snoleopard01001101@gmail.com>
2020-12-21 10:34:14 -0800

    Top home section display, card.js file create

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import "./Home.css"
 import Banner from "./Banner"
+import Card from "./Card"
 
 function Home() {
     return (
         <div className="home">
             <Banner />
+
+            <div className="home__section">
+                <Card src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im+w=720"
+                    title="Entire Homes"
+                    description="Comfortable private places, with room for friends or family" />
+                <Card src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im+w=720"
+                    title="Entire Homes"
+                    description="Comfortable private places, with room for friends or family" />
+                <Card src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im+w=720"
+                    title="Entire Homes"
+                    description="Comfortable private places, with room for friends or family" />
+    
+            </div>
+
+            <div className="home__section">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
