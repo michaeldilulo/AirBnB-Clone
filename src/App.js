6b05f40d29467ebfae9819ec62de948749830473
michael dilulo <snoleopard01001101@gmail.com>
2020-12-28 11:12:30 -0800

    React Routing Dom for Search & Home

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import './App.css';
 import Home from "./Home"
 import Header from "./Header"
 import Footer from "./Footer"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
 
-      <Header />
+      <Router>
 
-      <Home />
+        <Header />
+        
+        <Switch>
       
+          <Route path="/">
+          <Home />
+          </Route>
+
+          <Route path="/search">
+          <SearchPage />
+          </Route>
+
+        </Switch>
+
       <Footer />
-      
-      {/* Search Page / */}
+
+      </Router>
+
     </div>
   );
 }
