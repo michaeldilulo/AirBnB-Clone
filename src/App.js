72a45a857c6b96a62a75dfd468c830a33f6c3bee
michael dilulo <snoleopard01001101@gmail.com>
2020-12-28 11:14:09 -0800

    Route Path, SearchPage.js import, & CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from "./Home"
 import Header from "./Header"
 import Footer from "./Footer"
+import SearchPage from "./SearchPage"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
