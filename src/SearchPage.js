dca35a2f9f8ad73a8a9374e0564ac0e2541699e5
michael dilulo <snoleopard01001101@gmail.com>
2020-12-30 12:51:20 -0800

    Cleanup of H1 on the Search Page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -6,7 +6,6 @@ import SearchResult from "./SearchResult"
 function SearchPage() {
     return (
         <div className="searchPage">
-            <h1>I am the search page</h1>
             <div className="searchPage__info">
                 <p>62 stays - 26 August to 30 August - 2 guests</p>
                 <h1>Stays nearby</h1>
