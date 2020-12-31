c2a50b75c925b9d0ccfcc09519eed5c4ff5f614d
michael dilulo <snoleopard01001101@gmail.com>
2020-12-31 12:17:55 -0800

    Airbnb logo when clicked goes to home page

--- src/Header.js
+++ src/Header.js
@@ -3,16 +3,20 @@ import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search"
 import LanguageIcon from "@material-ui/icons/Language"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import {Avatar} from "@material-ui/core"
+import { Avatar } from "@material-ui/core"
+import {Link} from "react-router-dom"
 
 function Header() {
     return (
         <div className="header">
-            <img
+            <Link>
+                            <img
                 className="header__icon"
                 src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
                 alt=""
             />
+            </Link>
+
 
             <div className="header__center">
                 <input type="text" />
