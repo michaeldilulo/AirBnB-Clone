7569c1310db738c94881d2e737647decee5b1a2c
michael dilulo <snoleopard01001101@gmail.com>
2020-12-31 12:20:52 -0800

    Explore Nearby takes you to search page

--- src/Banner.js
+++ src/Banner.js
@@ -2,8 +2,11 @@ import React, {useState} from 'react'
 import "./Banner.css"
 import { Button } from "@material-ui/core"
 import Search from "./Search"
+import {useHistory} from "react-router-dom"
 
 function Banner() {
+    // Browser keeps track of the history, pushes results
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false)
 
 
@@ -18,7 +21,7 @@ function Banner() {
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
-                <Button variant="outlined">Explore Nearby</Button>
+                <Button onClick={() => history.push('/search')} variant="outlined">Explore Nearby</Button>
             </div>
         </div>
     )
